8584171129026624a7f9881080e2aceeb26fef08
Anna Lozytska <anna.alisa.lozytska@gmail.com>
2016-08-10 11:07:52 +0300

    Created skeleton of layer class that provides presenters with data in convenient way

--- app/src/main/java/com/anna/lozytska/achievementstimer/util/Utils.java
+++ app/src/main/java/com/anna/lozytska/achievementstimer/util/Utils.java
@@ -1,6 +1,7 @@
 package com.anna.lozytska.achievementstimer.util;
 
 import java.util.Collection;
+import java.util.Map;
 
 /**
  * Created by alozytska on 31.07.16.
@@ -9,6 +10,10 @@ public class Utils {
 
     public static final int getSize(Collection<?> collection) {
         return collection != null ? collection.size() : 0;
+    }
+
+    public static final int getSize(Map<?, ?> map) {
+        return map != null ? map.size() : 0;
     }
 
     public static final int getSize(Object[] array) {
